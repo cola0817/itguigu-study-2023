285ced83b8e51e6443a0c6f5eb17b44ceda21eb3
cola0817 <cola0817@gmail.com>
2023-08-24 02:29:03 +0800

    根据无参构造函数创建 bean

---
+++ p01-springFrameword-quickstart/src/main/java/itguigu/study/component/HappyComponent.java
@@ -0,0 +1,13 @@
+package itguigu.study.component;
+
+/**
+ * 基于午餐构造函数创建 bean
+ */
+public class HappyComponent {
+    public HappyComponent() {
+    }
+
+    public String sayHello(){
+        return "Hello it is HappyComponent";
+    }
+}
