ac4e77ad6e946cd921e9bfcf4317a668c3f5b6da
cola0817 <cola0817jz@gmail.com>
2023-08-23 15:42:30 +0800

    init commit

---
+++ p01-springBoot-quickstart/src/main/java/itguigu/study/P01SpringBootQuickstartApplication.java
@@ -0,0 +1,13 @@
+package itguigu.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class P01SpringBootQuickstartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(P01SpringBootQuickstartApplication.class, args);
+    }
+
+}
---
+++ p01-springBoot-quickstart/src/test/java/itguigu/study/P01SpringBootQuickstartApplicationTests.java
@@ -0,0 +1,13 @@
+package itguigu.study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class P01SpringBootQuickstartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
